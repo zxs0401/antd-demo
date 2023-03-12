a4888d38e20940dfa7937710d68da292403ab346
zxs0401 <859509286@qq.com>
2023-03-13 01:19:39 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,12 +1,294 @@
-import React from 'react';
-import { Button } from 'antd';
-import 'antd/dist/reset.css';
+// // import React from 'react';
+// // import { Button } from 'antd';
+// // import 'antd/dist/reset.css';
+//  import './App.css';
+
+// // const App = () => (
+// //   <div className="App">
+// //     <Button type="primary">Button231</Button>
+// //   </div>
+// // );
+
+// // export default App;
+// import React from 'react';
+// import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+// import { Breadcrumb, Layout, Menu, theme,Carousel } from 'antd';
+// const { Header, Content, Footer,Sider } = Layout;
+// const items1 = ['1', '2', '3'].map((key) => ({
+//   key,
+//   label: `nav ${key}`,
+// }));
+// const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+//   const key = String(index + 1);
+//   return {
+//     key: `sub${key}`,
+//     icon: React.createElement(icon),
+//     label: `subnav ${key}`,
+//     children: new Array(4).fill(null).map((_, j) => {
+//       const subKey = index * 4 + j + 1;                      
+//       return {
+//         key: subKey,
+//         label: `option${subKey}`,
+//       };
+//     }),
+//   };
+// });
+// const App = () => {
+//   const {
+//     token: { colorBgContainer },
+//   } = theme.useToken();
+//   return (
+//     <Layout className="layout">
+//       <Header>
+//         <div className="logo" />
+//         <Menu
+//           theme="dark"
+//           mode="horizontal"
+//           defaultSelectedKeys={['2']}
+//           items={new Array(3).fill(null).map((_, index) => {
+//             const key = index + 1;
+//             return {
+//               key,
+//               label: `nav ${key}`,
+//             };
+//           })}
+//         />
+//       </Header>
+//       <Content
+//         style={{
+//           padding: '0 50px',
+//         }}
+//       >
+//         <Layout
+//           style={{
+//             padding: '0px 0',
+//             background: colorBgContainer,
+//           }}
+//         >
+//           <Sider
+//             style={{
+//               background: colorBgContainer,
+//             }}
+//             width={200}
+//           >
+//             <Menu
+//               mode="inline"
+//               defaultSelectedKeys={['1']}
+//               defaultOpenKeys={['sub1']}
+//               style={{
+//                 height: '100%',
+//               }}
+//               items={items2}
+//             />
+//           </Sider>
+//           <Content
+//             style={{
+//               padding: '0 24px',
+//               minHeight: 280,
+//             }}
+//           >
+            
+//             <Layout className="site-layout">
+       
+//         <Content
+//           style={{
+//             margin: '0 16px',
+//           }}
+//         >
+//           <Breadcrumb
+//             style={{
+//               margin: '16px 0',
+//             }}
+//           >
+//             <Breadcrumb.Item>Home</Breadcrumb.Item>
+//           <Breadcrumb.Item>List</Breadcrumb.Item>
+//           <Breadcrumb.Item>App</Breadcrumb.Item>
+//           </Breadcrumb>
+//           <div
+//             style={{
+//               padding: 24,
+//               minHeight: 360,
+//               background: colorBgContainer,
+//             }}
+//           >
+//     <Carousel afterChange={onChange}>
+//       <div>
+//         <h3 style={contentStyle}>1</h3>
+//       </div>
+//       <div>
+//         <h3 style={contentStyle}>2</h3>
+//       </div>
+//       <div>
+//         <h3 style={contentStyle}>3</h3>
+//       </div>
+//       <div>
+//         <h3 style={contentStyle}>4</h3>
+//       </div> 
+//     </Carousel>
+//          </div>
+//         </Content>
+//       <Footer
+//         style={{
+//           textAlign: 'center',
+//         }}
+//       >
+//         Ant Design ©2023 Created by Ant UED
+//       </Footer>
+//     </Layout>
+//           </Content>
+//         </Layout>
+//          {/* <div
+//           className="site-layout-content"
+//           style={{
+//             background: colorBgContainer,
+//           }}
+//         >
+//           Content
+//         </div>  */}
+//       </Content>
+      
+//     </Layout>
+//   );
+// };
+
+
+// export default App;
 import './App.css';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme ,Carousel} from 'antd';
+import React from 'react';
+//import { Carousel } from 'antd';
+import { Collapse } from 'antd';
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button231</Button>
-  </div>
-);
 
-export default App;
+const { Header, Content, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+const contentStyle = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+const { Panel } = Collapse;
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+
+
+
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+      </Header>
+      <Layout>
+        <Sider
+          width={200}
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{
+              height: '100%',
+              borderRight: 0,
+            }}
+            items={items2}
+          />
+        </Sider>
+        <Layout
+          style={{
+            padding: '0 24px 24px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+    <Carousel afterChange={onChange}>
+      <div>
+        <h3 style={contentStyle}>1</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>2</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>3</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>4</h3>
+      </div>
+    </Carousel>
+    <Collapse defaultActiveKey={['1']} onChange={onChange}>
+      <Panel header="This is panel header 1" key="1">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 2" key="2">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 3" key="3">
+        <p>{text}</p>
+      </Panel>
+    </Collapse>             
+          </Content>
+          
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+
+  
+  
+};
+
+
+export default App; 
